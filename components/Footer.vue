e764e17854101f7287ec9113f9b8269958e70668
manolobrown <manny3279@gmail.com>
2022-09-19 18:36:53 -0400

    [UPDATE] Tablet Home Layout

--- components/Footer.vue
+++ components/Footer.vue
@@ -19,12 +19,12 @@ const footerLinks = [
 ];
 </script>
 <template>
-  <div
-    class="before:block before:absolute before:bg-[#D87D4A] before:h-1 before:top-0 before:w-[101px] before:left-1/2 before:-translate-x-1/2 bg-black text-center pt-[52px] pb-[38px] relative"
-  >
-    <div class="container">
+  <div class="bg-black text-center pt-[52px] pb-[38px] md:text-left">
+    <div
+      class="container before:block before:absolute before:bg-[#D87D4A] before:h-1 before:top-[-52px] before:w-[101px] before:left-1/2 before:-translate-x-1/2 relative md:before:left-[2.5rem] md:before:translate-x-0"
+    >
       <svg
-        class="mx-auto mb-[48px]"
+        class="mx-auto mb-[48px] md:ml-0"
         width="143"
         height="25"
         xmlns="http://www.w3.org/2000/svg"
@@ -35,7 +35,9 @@ const footerLinks = [
           fill-rule="nonzero"
         />
       </svg>
-      <ul class="flex flex-col items-center justify-center gap-y-4 mb-[48px]">
+      <ul
+        class="flex flex-col items-center justify-center gap-y-4 mb-[48px] md:flex-row md:gap-x-[34px] md:justify-start"
+      >
         <li v-for="footerLink in footerLinks">
           <a
             class="text-white uppercase text-[13px] leading-[25px] tracking-[2px] transition-colors hover:text-[#D87D4A]"
@@ -50,44 +52,48 @@ const footerLinks = [
         helping you get the most out of personal audio. Come and visit our demo
         facility - we’re open 7 days a week.
       </p>
-      <div class="text-white text-[15px] leading-[25px] opacity-50 mb-[48px]">
-        Copyright 2021. All Rights Reserved
+      <div class="md:flex md:justify-between md:items-center">
+        <div
+          class="text-white text-[15px] leading-[25px] opacity-50 mb-[48px] md:mb-0"
+        >
+          Copyright 2021. All Rights Reserved
+        </div>
+        <ul class="flex items-center justify-center gap-x-4">
+          <li>
+            <a href=""
+              ><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  class="transition-[fill]"
+                  d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z"
+                  fill="#FFF"
+                  fill-rule="nonzero"
+                /></svg
+            ></a>
+          </li>
+          <li>
+            <a href=""
+              ><svg width="24" height="20" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  class="transition-[fill]"
+                  d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
+                  fill="#FFF"
+                  fill-rule="nonzero"
+                /></svg
+            ></a>
+          </li>
+          <li>
+            <a href=""
+              ><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  class="transition-[fill]"
+                  d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"
+                  fill="#FFF"
+                  fill-rule="nonzero"
+                /></svg
+            ></a>
+          </li>
+        </ul>
       </div>
-      <ul class="flex items-center justify-center gap-x-4">
-        <li>
-          <a href=""
-            ><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-              <path
-                class="transition-[fill]"
-                d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z"
-                fill="#FFF"
-                fill-rule="nonzero"
-              /></svg
-          ></a>
-        </li>
-        <li>
-          <a href=""
-            ><svg width="24" height="20" xmlns="http://www.w3.org/2000/svg">
-              <path
-                class="transition-[fill]"
-                d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
-                fill="#FFF"
-                fill-rule="nonzero"
-              /></svg
-          ></a>
-        </li>
-        <li>
-          <a href=""
-            ><svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
-              <path
-                class="transition-[fill]"
-                d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"
-                fill="#FFF"
-                fill-rule="nonzero"
-              /></svg
-          ></a>
-        </li>
-      </ul>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <script setup></script>
 <template>
   <div>
-    <Hero />
+    <Hero class="sm:mb-[84px]" />
     <NavsCategoryNav class="mb-[84px]" />
     <div class="container">
       <div
-        class="bg-[#D87D4A] bg-[url('/assets/images/home/desktop/pattern-circles.svg')] bg-cover bg-no-repeat bg-[center_top_-135px] rounded-lg text-center px-[24px] py-[55px] mb-[24px]"
+        class="bg-[#D87D4A] bg-[url('/assets/images/home/desktop/pattern-circles.svg')] bg-cover bg-no-repeat bg-[center_top_-135px] rounded-lg text-center px-[24px] py-[55px] mb-[24px] md:h-[720px] md:flex md:items-center md:justify-center md:flex-col md:bg-auto md:bg-[center_top_-300px]"
       >
-        <div class="mb-[32px]">
+        <div class="mb-[32px] md:mb-[64px]">
           <img
             class="mx-auto max-h-[207px]"
             src="~/assets/images/home/mobile/image-speaker-zx9.png"
@@ -15,11 +15,13 @@
           />
         </div>
         <h2
-          class="text-white uppercase text-[40px] tracking-[1.29px] leading-[40px] mb-[24px]"
+          class="text-white uppercase text-[40px] tracking-[1.29px] leading-[40px] mb-[24px] md:text-[56px] md:leading-[50px] md:tracking-[2px]"
         >
           <strong><span class="block">ZX9</span> Speaker</strong>
         </h2>
-        <p class="text-white text-[15px] leading-[25px] opacity-75 mb-[24px]">
+        <p
+          class="text-white text-[15px] leading-[25px] opacity-75 mb-[24px] max-w-[349px] mx-auto md:mb-[40px]"
+        >
           Upgrade to premium speakers that are phenomenally built to deliver
           truly remarkable sound.
         </p>
@@ -31,7 +33,7 @@
       </div>
       <!-- new card -->
       <div
-        class="bg-[#F1F1F1] bg-[url('/assets/images/home/mobile/image-speaker-zx7.jpg')] bg-cover rounded-lg text-center px-[24px] py-[55px] mb-[24px] h-[320px] flex items-start justify-center flex-col"
+        class="bg-[#F1F1F1] bg-[url('/assets/images/home/mobile/image-speaker-zx7.jpg')] bg-cover rounded-lg text-center px-[24px] py-[55px] mb-[24px] h-[320px] flex items-start justify-center flex-col md:px-[62px] md:bg-[url('/assets/images/home/tablet/image-speaker-zx7.jpg')]"
       >
         <h2
           class="text-black uppercase text-[28px] tracking-[2px] leading-[40px] mb-[32px]"
@@ -46,12 +48,14 @@
         />
       </div>
       <!-- new card -->
-      <div>
+      <div
+        class="mb-[120px] md:mb-[72px] md:grid md:grid-cols-2 md:gap-x-[10px]"
+      >
         <div
-          class="bg-[#F1F1F1] bg-[url('/assets/images/home/mobile/image-earphones-yx1.jpg')] bg-cover rounded-lg text-center px-[24px] mb-[24px] h-[200px] flex items-start justify-center flex-col"
+          class="bg-[#F1F1F1] bg-[url('/assets/images/home/mobile/image-earphones-yx1.jpg')] bg-cover rounded-lg text-center px-[24px] mb-[24px] h-[200px] flex items-start justify-center flex-col md:bg-[url('/assets/images/home/tablet/image-earphones-yx1.jpg')] md:h-[320px]"
         ></div>
         <div
-          class="bg-[#F1F1F1] rounded-lg text-center px-[24px] mb-[120px] h-[200px] flex items-start justify-center flex-col"
+          class="bg-[#F1F1F1] rounded-lg text-center px-[24px] h-[200px] flex items-start justify-center flex-col md:h-[320px] md:px-[40px]"
         >
           <h2
             class="text-black uppercase text-[28px] tracking-[2px] leading-[40px] mb-[32px]"
@@ -69,19 +73,33 @@
       <!-- new card -->
       <div class="text-center mb-[120px]">
         <div class="mb-[40px]">
-          <img
-            class="mx-auto rounded-lg"
-            src="~/assets/images/shared/mobile/image-best-gear.jpg"
-            alt=""
-          />
+          <picture>
+            <source
+              srcset="~/assets/images/shared/desktop/image-best-gear.jpg"
+              media="(min-width:1200px)"
+            />
+            <source
+              srcset="~/assets/images/shared/tablet/image-best-gear.jpg"
+              media="(min-width:768px)"
+            />
+            <img
+              class="mx-auto rounded-lg"
+              src="~/assets/images/shared/mobile/image-best-gear.jpg"
+              alt=""
+            />
+          </picture>
         </div>
-        <h2 class="text-black uppercase text-[28px] tracking-[1px] mb-[32px]">
+        <h2
+          class="text-black uppercase text-[28px] tracking-[1px] mb-[32px] md:text-[40px] md:tracking-[1.43px]md:leading-[44px] max-w-[573px] mx-auto"
+        >
           <strong
             >Bringing you the <span class="text-[#D87D4A]">best</span> audio
             gear</strong
           >
         </h2>
-        <p class="text-black text-[15px] leading-[25px] opacity-50 mb-[24px]">
+        <p
+          class="text-black text-[15px] leading-[25px] opacity-50 mb-[24px] max-w-[573px] mx-auto md:mx-2"
+        >
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
