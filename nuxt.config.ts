69a6f9db4624c048eced8aeabe2ccabb81245d49
manolobrown <manny3279@gmail.com>
2022-09-23 19:33:01 -0400

    [UPDATE] Product Detail Page
    -mobile layout completed
    -code clean up
    -FormKit package added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "@formkit/nuxt"],
   css: ["@/assets/css/main.css"],
   content: [
     "./components/**/*.{js,vue,ts}",
--- pages/[product]/[slug].vue
+++ pages/[product]/[slug].vue
@@ -4,6 +4,7 @@ const route = useRoute();
 const router = useRouter();
 const productSlug = route.params.slug;
 const singleProduct = data.find((product) => product.slug === productSlug);
+const gallery = Object.values(singleProduct.gallery);
 const goBack = () => {
   router.back();
 };
@@ -20,6 +21,7 @@ const goBack = () => {
       <div
         class="text-left mb-[120px] lg:flex lg:even:flex-row-reverse lg:items-center lg:gap-x-[125px] lg:text-left lg:mb-40"
       >
+        <!-- product card -->
         <div class="lg:flex-auto lg:max-w-[540px]">
           <picture>
             <source
@@ -37,7 +39,7 @@ const goBack = () => {
             />
           </picture>
         </div>
-        <div>
+        <div class="mb-[88px]">
           <div
             v-if="singleProduct.new"
             class="uppercase text-[#D87D4A] text-[14px] tracking-[10px] mb-6 lg:mb-4"
@@ -62,6 +64,75 @@ const goBack = () => {
             :button-url="`/${singleProduct.category}/${singleProduct.slug}`"
             class="btn--primary mx-auto lg:mx-0"
           />
+        </div>
+        <!-- features -->
+        <h3
+          class="font-bold text-[24px] leading-[36px] tracking-[0.86px] text-black uppercase mb-6"
+        >
+          Features
+        </h3>
+        <div class="text-black opacity-50 text-[15px] leading-[25px] mb-[88px]">
+          {{ singleProduct.features }}
+        </div>
+        <!-- in the box -->
+        <h3
+          class="font-bold text-[24px] leading-[36px] tracking-[0.86px] text-black uppercase mb-6"
+        >
+          In The Box
+        </h3>
+        <ul class="text-[15px] leading-[25px] flex flex-col gap-y-2 mb-[88px]">
+          <li class="flex gap-x-6" v-for="include in singleProduct.includes">
+            <span class="text-[#D87D4A] font-bold"
+              >{{ include.quantity }}X</span
+            >
+            <span class="opacity-50">{{ include.item }}</span>
+          </li>
+        </ul>
+        <!-- gallery -->
+        <div class="grid gap-6 mb-[120px]">
+          <picture v-for="image in gallery">
+            <source :srcset="image.desktop" media="(min-width:1280px)" />
+            <source :srcset="image.tablet" media="(min-width:768px)" />
+            <img class="rounded-lg" :src="image.mobile" alt="" />
+          </picture>
+        </div>
+        <!-- related products -->
+        <h3
+          class="font-bold text-[24px] text-center leading-[36px] tracking-[0.86px] text-black uppercase mb-10"
+        >
+          You May Also Like
+        </h3>
+        <div class="flex gap-y-14 flex-col">
+          <div v-for="related in singleProduct.others">
+            <picture>
+              <source
+                :srcset="related.image.desktop"
+                media="(min-width:1280px)"
+              />
+              <source
+                :srcset="related.image.tablet"
+                media="(min-width:768px)"
+              />
+              <img
+                class="rounded-lg mb-8 md:mb-[52px]"
+                :src="related.image.mobile"
+                alt=""
+              />
+            </picture>
+            <div class="mb-0">
+              <h4
+                class="uppercase font-bold text-[28px] text-center tracking-[1px] mb-8 max-w-[270px] mx-auto md:text-[40px] md:leading-[44px] md:tracking-[1.43px] md:max-w-[300px] md:mb-8 lg:mx-0"
+              >
+                {{ related.name }}
+              </h4>
+
+              <ButtonsButtonOne
+                button-text="See Product"
+                :button-url="`/${route.params.product}/${related.slug}`"
+                class="btn--primary mx-auto lg:mx-0 mb-0"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
